cc73a909b3897c6ab448933e6388ba06770ace86
walonjalloh <mohamedlaminwalonjalloh@gmail.com>
2024-08-27 22:47:23 +0000

    minior edit

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -11,8 +11,8 @@ function AboutUs(){
           <h1 className="text-3xl font-bold my-2">About Us</h1>
           <p className="text-center text-xl font-serif font-thin mx-auto max-w-[540px]">
             Welcome to Learn X, where we are reshaping the future of education.
-            Founded in 2020, we have been at the forefront of educational
-            innovation, serving over 1 million learners across 50 countries.
+            Founded in 2023, we have been at the forefront of educational
+            innovation, serving over 200 thousand learners across 15 countries.
           </p>
         </div>
         <div className="flex flex-col justify-center">
@@ -28,15 +28,15 @@ function AboutUs(){
             </div>
             <div className="flex flex-col gap-4  md:flex-row md:gap-[200px] mt-5">
               <div className="flex flex-col flex-1  text-center">
-                <h3 className="font-bold text-2xl ">1M+</h3>
+                <h3 className="font-bold text-2xl ">200K+</h3>
                 <p className="text-xl font-thin">Learners</p>
               </div>
               <div className="flex flex-col text-center flex-1">
-                <h3 className="font-bold text-2xl ">50+</h3>
+                <h3 className="font-bold text-2xl ">15+</h3>
                 <p className="font-thin text-xl">Countries</p>
               </div>
               <div className="flex flex-col text-center flex-1">
-                <h3 className="font-bold text-2xl">100+</h3>
+                <h3 className="font-bold text-2xl">15+</h3>
                 <p className="font-thin text-xl">Courses</p>
               </div>
             </div>  
@@ -46,7 +46,7 @@ function AboutUs(){
         <div className="flex flex-col mt-10 text-center items-center">
           <h2 className="text-3xl font-bold font-mono ">Join Our Learning Community</h2>
           <p className="text-xl font-thin mx-2">Experience the future of education today!</p>
-          <Button name="Get Started" classname='text-white bg-black px-4 py-2  rounded-md mt-2 ' />
+          <Button name="Get Started" classname='text-white bg-black px-4 py-2  rounded-md mt-4 ' />
         </div>
       </div>
     </section>
